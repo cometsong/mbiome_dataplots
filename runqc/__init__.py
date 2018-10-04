5623aee06630d6afd4e1a11de6e99d2c17121e21
cometsong <benjamin@cometsong.net>
2018-10-04 15:35:37 -0400

    version bump for plot additions

--- runqc/__init__.py
+++ runqc/__init__.py
@@ -1,5 +1,5 @@
 # __all__ = ['app', 'config', 'views', 'db', 'models', 'files']
-__version_bits__ = (1, 0, 1)
+__version_bits__ = (1, 1, 0)
 __version__ = ".".join(map(str,__version_bits__))
 
 import os

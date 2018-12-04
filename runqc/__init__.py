44bdcf91b9b50e28abb77ed89a8f092cc206ed64
cometsong <benjamin@cometsong.net>
2018-12-04 10:27:09 -0500

    v1.2.3 update

--- runqc/__init__.py
+++ runqc/__init__.py
@@ -1,5 +1,5 @@
 # __all__ = ['app', 'config', 'views', 'db', 'models', 'files']
-__version_bits__ = (1, 2, 2)
+__version_bits__ = (1, 2, 3)
 __version__ = ".".join(map(str,__version_bits__))
 
 import os
